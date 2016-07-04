0bfb165d6325d082b721537698ee89dc43e9f5c1
Jennifer Atherton <jenatherton03@gmail.com>
2016-07-04 12:25:24 +0100

    Linking to files in other directories

--- 04_twython.py
+++ 04_twython.py
@@ -1,13 +1,18 @@
 """With help from the Twitter example from
 http://pythoncentral.io/how-to-use-the-twython-twitter-python-library/
 """
-
 from twython import Twython
 
-CONSUMER_KEY = ""
-CONSUMER_SECRET = ""
-ACCESS_KEY = ""
-ACCESS_SECRET = ""
+#Bringing in my personal twitter keys
+with open("C:/Users/Jen/Documents/Python for Windows/twitter_auth.py") as f:
+    code = compile(f.read(), "C:/Users/Jen/Documents/Python for Windows/twitter_auth.py" , 'exec')
+    exec(code)
+
+CONSUMER_KEY = auth.ckey
+CONSUMER_SECRET = auth.csecret
+ACCESS_KEY = auth.accessk
+ACCESS_SECRET = auth.accesss
+
 
 t=Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
 
@@ -20,7 +25,5 @@ for tweet in user_timeline:
 
 home_timeline=t.get_home_timeline(count=5)
 print("\n\nHome timeline")
-print(home_timeline)
-# for tweet in home_timeline:
-#     print(tweet)
-    #print("\nTweet:",tweet['text'],"\nUser:",tweet[''])
+for tweet in home_timeline:
+    print(tweet['text'])
